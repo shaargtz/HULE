6564e356d70ba5faa36000580bbf55530af86777
Mariano Shaar <a00825287@itesm.mx>
2023-04-27 17:20:22 -0600

    Cubo semantico

---
+++ cubo_semantico.py
@@ -0,0 +1,48 @@
+class CuboSemantico:
+    def __init__(self):
+        self.cubo = {
+            ('+', 'ent', 'ent')         : 'ent',
+            ('+', 'ent', 'flot')        : 'flot',
+            ('+', 'flot', 'ent')        : 'flot',
+            ('+', 'flot', 'flot')       : 'flot',
+            ('-', 'ent', 'ent')         : 'ent',
+            ('-', 'ent', 'flot')        : 'flot',
+            ('-', 'flot', 'ent')        : 'flot',
+            ('-', 'flot', 'flot')       : 'flot',
+            ('*', 'ent', 'ent')         : 'ent',
+            ('*', 'ent', 'flot')        : 'flot',
+            ('*', 'flot', 'ent')        : 'flot',
+            ('*', 'flot', 'flot')       : 'flot',
+            ('/', 'ent', 'ent')         : 'ent',
+            ('/', 'ent', 'flot')        : 'flot',
+            ('/', 'flot', 'ent')        : 'flot',
+            ('/', 'flot', 'flot')       : 'flot',
+            ('&', 'bool', 'bool')       : 'bool',
+            ('|', 'bool', 'bool')       : 'bool',
+            ('>', 'ent', 'ent')         : 'bool',
+            ('>', 'ent', 'flot')        : 'bool',
+            ('>', 'flot', 'ent')        : 'bool',
+            ('>', 'flot', 'flot')       : 'bool',
+            ('<', 'ent', 'ent')         : 'bool',
+            ('<', 'ent', 'flot')        : 'bool',
+            ('<', 'flot', 'ent')        : 'bool',
+            ('<', 'flot', 'flot')       : 'bool',
+            ('==', 'ent', 'ent')        : 'bool',
+            ('==', 'ent', 'flot')       : 'bool',
+            ('==', 'flot', 'ent')       : 'bool',
+            ('==', 'flot', 'flot')      : 'bool',
+            ('==', 'car', 'car')        : 'bool',
+            ('==', 'cadena', 'cadena')  : 'bool',
+            ('==', 'bool', 'bool')      : 'bool',
+            ('!=', 'ent', 'ent')        : 'bool',
+            ('!=', 'ent', 'flot')       : 'bool',
+            ('!=', 'flot', 'ent')       : 'bool',
+            ('!=', 'flot', 'flot')      : 'bool',
+            ('!=', 'car', 'car')        : 'bool',
+            ('!=', 'cadena', 'cadena')  : 'bool',
+            ('!=', 'bool', 'bool')      : 'bool',
+        }
+
+    def emparejar_tipo(self, tupla):
+        tipo = self.cubo.get(tupla)
+        return tipo if tipo else 'incompatibles'
