aa943e900d1e87e495828497fb28e4ea0c4ca80b
Mariano Shaar <a00825287@itesm.mx>
2023-04-27 18:06:44 -0600

    Tabla de variables

---
+++ tabla_variables.py
@@ -0,0 +1,25 @@
+class Variables:
+    def __init__(self, es_global):
+        # nombre : (tipo, direccion)
+        self.tabla = {}
+        if (es_global):
+            self.contadores = {
+                'ent' : 0,
+                'flot' : 1000,
+                'car' : 2000,
+                'cadena' : 3000,
+                'bool' : 4000
+            }
+        else:
+            self.contadores = {
+                'ent' : 5000,
+                'flot' : 6000,
+                'car' : 7000,
+                'cadena' : 8000,
+                'bool' : 9000
+            }
+
+    def insertar_variable(self, var, tipo):
+        if (not self.tabla.get(var)):
+            self.tabla[var] = (tipo, self.contadores[tipo])
+            self.contadores[tipo] = self.contadores[tipo] + 1
