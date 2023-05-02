624ba0e86dfb43b2f66a9606ec2fb95726d625b8
Mariano Shaar <a00825287@itesm.mx>
2023-05-02 14:55:06 -0600

    Formateo de strings en maquina virtual

--- maquina_virtual.py
+++ maquina_virtual.py
@@ -16,111 +16,111 @@ class MaquinaVirtual:
             destino = self.cuadruplos[contador][3]
             mem = self.memoria.memoria
             if operador == '+':
-                print("{} + {} = ".format(mem[operando_1], mem[operando_2]) + str(mem[operando_1] + mem[operando_2]))
+                print("{} + {} = ".format(operando_1, operando_2) + str(mem[operando_1] + mem[operando_2]))
                 mem[destino] = mem[operando_1] + mem[operando_2]
             elif operador == '-':
-                print("{mem[operando_1]} - {mem[operando_2]} = " + str(mem[operando_1] - mem[operando_2]))
+                print("{} - {} = ".format(operando_1, operando_2) + str(mem[operando_1] - mem[operando_2]))
                 mem[destino] = mem[operando_1] - mem[operando_2]
             elif operador == '*':
-                print("{mem[operando_1]} * {mem[operando_2]} = " + str(mem[operando_1] * mem[operando_2]))
+                print("{} * {} = ".format(operando_1, operando_2) + str(mem[operando_1] * mem[operando_2]))
                 mem[destino] = mem[operando_1] * mem[operando_2]
             elif operador == '/':
-                print("{mem[operando_1]} / {mem[operando_2]} = " + str(mem[operando_1] / mem[operando_2]))
+                print("{} / {} = ".format(operando_1, operando_2) + str(mem[operando_1] / mem[operando_2]))
                 mem[destino] = mem[operando_1] / mem[operando_2]
             elif operador == '&':
-                print("{mem[operando_1]} & {mem[operando_2]} = " + str(mem[operando_1] and mem[operando_2]))
+                print("{} & {} = ".format(operando_1, operando_2) + str(mem[operando_1] and mem[operando_2]))
                 mem[destino] = mem[operando_1] and mem[operando_2]
             elif operador == '|':
-                print("{mem[operando_1]} | {mem[operando_2]} = " + str(mem[operando_1] or mem[operando_2]))
+                print("{} | {} = ".format(operando_1, operando_2) + str(mem[operando_1] or mem[operando_2]))
                 mem[destino] = mem[operando_1] or mem[operando_2]
             elif operador == '>':
-                print("{mem[operando_1]} > {mem[operando_2]} = " + str(mem[operando_1] > mem[operando_2]))
+                print("{} > {} = ".format(operando_1, operando_2) + str(mem[operando_1] > mem[operando_2]))
                 mem[destino] = mem[operando_1] > mem[operando_2]
             elif operador == '<':
-                print("{mem[operando_1]} < {mem[operando_2]} = " + str(mem[operando_1] < mem[operando_2]))
+                print("{} < {} = ".format(operando_1, operando_2) + str(mem[operando_1] < mem[operando_2]))
                 mem[destino] = mem[operando_1] < mem[operando_2]
             elif operador == '==':
-                print("{mem[operando_1]} == {mem[operando_2]} = " + str(mem[operando_1] == mem[operando_2]))
+                print("{} == {} = ".format(operando_1, operando_2) + str(mem[operando_1] == mem[operando_2]))
                 mem[destino] = mem[operando_1] == mem[operando_2]
             elif operador == '!=':
-                print("{mem[operando_1]} != {mem[operando_2]} = " + str(mem[operando_1] != mem[operando_2]))
+                print("{} != {} = ".format(operando_1, operando_2) + str(mem[operando_1] != mem[operando_2]))
                 mem[destino] = mem[operando_1] != mem[operando_2]
             elif operador == '=':
-                print("{} = {}".format(destino, mem[operando_1]))
+                print("{} = {}".format(destino, operando_1))
                 mem[destino] = mem[operando_1]
             elif operador == 'imprime':
-                print("imprime({mem[operando_1]})")
+                print("imprime({})".format(operando_1))               
                 print(mem[operando_1])
             elif operador == 'sen':
-                print("sen({mem[operando_1]}) = " + str(math.sen(mem[operando_1])))
+                print("sen({}) = ".format(operando_1) + str(math.sen(mem[operando_1])))
                 mem[destino] = math.sen(mem[operando_1])
             elif operador == 'cos':
-                print("cos({mem[operando_1]}) = " + str(math.cos(mem[operando_1])))
+                print("cos({}) = ".format(operando_1) + str(math.cos(mem[operando_1])))
                 mem[destino] = math.cos(mem[operando_1])
             elif operador == 'tan':
-                print("tan({mem[operando_1]}) = " + str(math.tan(mem[operando_1])))
+                print("tan({}) = ".format(operando_1) + str(math.tan(mem[operando_1])))
                 mem[destino] = math.tan(mem[operando_1])
             elif operador == 'senh':
-                print("senh({mem[operando_1]}) = " + str(math.senh(mem[operando_1])))
+                print("senh({}) = ".format(operando_1) + str(math.senh(mem[operando_1])))
                 mem[destino] = math.senh(mem[operando_1])
             elif operador == 'cosh':
-                print("cosh({mem[operando_1]}) = " + str(math.cosh(mem[operando_1])))
+                print("cosh({}) = ".format(operando_1) + str(math.cosh(mem[operando_1])))
                 mem[destino] = math.cosh(mem[operando_1])
             elif operador == 'tanh':
-                print("tanh({mem[operando_1]}) = " + str(math.tanh(mem[operando_1])))
+                print("tanh({}) = ".format(operando_1) + str(math.tanh(mem[operando_1])))
                 mem[destino] = math.tanh(mem[operando_1])
             elif operador == 'min':
-                print("min({mem[operando_1]}, {mem[operando_2]}) = " + str(min(mem[operando_1], mem[operando_2])))
+                print("min({}, {}) = ".format(operando_1, operando_2) + str(min(mem[operando_1], mem[operando_2])))
                 mem[destino] = min(mem[operando_1], mem[operando_2])
             elif operador == 'max':
-                print("max({mem[operando_1]}, {mem[operando_2]}) = " + str(max(mem[operando_1], mem[operando_2])))
+                print("max({}, {}) = ".format(operando_1, operando_2) + str(max(mem[operando_1], mem[operando_2])))
                 mem[destino] = max(mem[operando_1], mem[operando_2])
             elif operador == 'largo':
                 # por hacer listas
-                print("largo de la lista {operando_1} = ")
+                print("largo de la lista {} = ".format(operando_1))
             elif operador == 'media':
                 # por hacer listas
-                print("media de la lista {operando_1} = ")
+                print("media de la lista {} = ".format(operando_1))
             elif operador == 'moda':
                 # por hacer listas
-                print("moda de la lista {operando_1} = ")
+                print("moda de la lista {} = ".format(operando_1))
             elif operador == 'mediana':
                 # por hacer listas
-                print("mediana de la lista {operando_1} = ")
+                print("mediana de la lista {} = ".format(operando_1))
             elif operador == 'piso':
-                print("piso({mem[operando_1]}) = " + str(math.floor(mem[operando_1])))
+                print("piso({}) = ".format(operando_1) + str(math.floor(mem[operando_1])))
                 mem[destino] = math.floor(mem[operando_1])
             elif operador == 'techo':
-                print("techo({mem[operando_1]}) = " + str(math.ceil(mem[operando_1])))
+                print("techo({}) = ".format(operando_1) + str(math.ceil(mem[operando_1])))
                 mem[destino] = math.ceil(mem[operando_1])
             elif operador == 'aleatorio':
                 rand = random.randint()
-                print("aleatorio() = {rand}")
+                print("aleatorio() = {}".format(rand))
                 mem[destino] = rand
             elif operador == 'poder':
-                print("{mem[operando_1]} ^ {mem[operando_2]} = " + str(pow(mem[operando_1], mem[operando_2])))
+                print("{} ^ {} = ".format(operando_1, operando_2) + str(pow(mem[operando_1], mem[operando_2])))
                 mem[destino] = pow(mem[operando_1], mem[operando_2])
             elif operador == 'log':
-                print("{mem[operando_1]} log {mem[operando_2]} = " + str(math.log(mem[operando_1], mem[operando_2])))
+                print("{} log {} = ".format(operando_1, operando_2) + str(math.log(mem[operando_1], mem[operando_2])))
                 mem[destino] = math.log(mem[operando_1], mem[operando_2])
             elif operador == 'abs':
-                print("abs({mem[operando_1]}) = " + str(abs(mem[operando_1])))
+                print("abs({}) = ".format(operando_1) + str(abs(mem[operando_1])))
                 mem[destino] = abs(mem[operando_1])
             elif operador == 'sec':
                 # por hacer listas
-                print("sec({mem[operando_1]}, mem[operando_1]}) = ")
+                print("sec({}) = ".format(operando_1))
             elif operador == 'GOTOF':
-                print("GOTOF {mem[operando_1]} {destino}")
+                print("GOTOF {} {}".format(operando_1, destino))
                 if (not mem[operando_1]):
                     contador = destino - 1
             elif operador == 'GOTOV':
-                print("GOTOV {mem[operando_1]} {destino}")
+                print("GOTOV {} {}".format(operando_1, destino))
                 if (mem[operando_1]):
                     contador = destino - 1
             elif operador == 'GOTO':
-                print("GOTOF {destino}")
+                print("GOTOF {}".format(destino))
                 contador = destino - 1
             else:
-                print("Operador no reconocido: {operador}")
+                print("Operador no reconocido: {}".format(operador))
             
             contador = contador + 1
