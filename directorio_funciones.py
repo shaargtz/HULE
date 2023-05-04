f0d42afd7f693a3957db59aadd58ce4419b31af9
Mariano Shaar <a00825287@itesm.mx>
2023-05-03 18:47:57 -0600

    Cambio en nombre de funcion global y tabla de variables

--- directorio_funciones.py
+++ directorio_funciones.py
@@ -4,9 +4,9 @@ class DirFunciones:
     def __init__(self):
         self.directorio = {
             # nombre : [tipo, tabla de variables]
-            'global' : ['void', Variables(True)]
+            'hule' : ['void', Variables('global')]
         }
     
     def insertar_funcion(self, func, tipo):
-        if (not self.directorio.get(func)):
-            self.directorio[func] = [tipo, Variables(False)]
+        if not self.directorio.get(func):
+            self.directorio[func] = [tipo, Variables('local')]
