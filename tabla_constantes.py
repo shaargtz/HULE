565aac8b98c72a40e836929b5cdde39dc2800e28
Mariano Shaar <a00825287@itesm.mx>
2023-05-04 21:08:38 -0600

    Cambio en el retorno y comentarios

--- tabla_constantes.py
+++ tabla_constantes.py
@@ -1,6 +1,8 @@
 class Constantes:
     def __init__(self):
         self.tabla = {}
+        # cada contador tiene el valor del siguiente espacio de memoria para cada tipo
+        # tambien, al hacer modulo 1000 se consigue la cantidad de variables en cada tipo
         self.contadores = {
             'ent' : 20000,
             'flot' : 21000,
@@ -13,5 +15,5 @@ class Constantes:
         if not self.tabla.get(var):
             self.tabla[var] = (tipo, self.contadores[tipo])
             self.contadores[tipo] = self.contadores[tipo] + 1
-            return self.contadores[tipo]
+            return self.contadores[tipo] - 1
         return -1
