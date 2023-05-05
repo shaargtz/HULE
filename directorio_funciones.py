3b569d0372b700012163003e8836da0bdb375381
Mariano Shaar <a00825287@itesm.mx>
2023-05-04 21:09:06 -0600

    Abstraccion para buscar una variable

--- directorio_funciones.py
+++ directorio_funciones.py
@@ -10,3 +10,10 @@ class DirFunciones:
             self.directorio[func] = [tipo, None, []]
         else:
             raise Exception("Funcion " + func + " definida multiples veces")
+        
+    def buscar_variable(self, func, id):
+        var = self.directorio[func][1].tabla['var'].get(id)
+        if var:
+            return var[1]
+        else:
+            raise Exception("Variable " + id + " no definida")
