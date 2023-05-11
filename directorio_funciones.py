56e9e7d82a64a0dfa19048cd6d1060c5155e3038
Mariano Shaar <a00825287@itesm.mx>
2023-05-11 17:04:09 -0600

    Abstraccion de codigo con funciones

--- directorio_funciones.py
+++ directorio_funciones.py
@@ -1,3 +1,5 @@
+from tabla_variables import Variables
+
 memoria_vacia = {
                     'total' : 0,
                     'ent' : 0,
@@ -14,15 +16,27 @@ class DirFunciones:
             'hule' : ['void', None, [], memoria_vacia, None, None]
         }
     
-    def insertar(self, func, tipo):
+    def insertar_funcion(self, func, tipo):
         if not self.directorio.get(func):
             self.directorio[func] = [tipo, None, [], memoria_vacia, None, None]
         else:
             raise Exception("Funcion " + func + " definida multiples veces")
         
+    def insertar_tabla_var(self, func, alcance):
+        self.directorio[func][1] = Variables(alcance)
+        
+    def insertar_variable(self, func, tipo, var=None):
+        self.directorio[func][1].insertar(tipo, var)
+
+    def insertar_parametro(self, func, tipo):
+        self.directorio[func][2].append(tipo)
+
     def buscar_variable(self, func, id):
         var = self.directorio[func][1].tabla['var'].get(id)
         if var:
             return var[1]
         else:
             raise Exception("Variable " + id + " no definida")
+        
+    def tiene_tabla_var(self, func):
+        return self.directorio[func][1] != None
