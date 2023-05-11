1f40d4c4b41603fd872f5847a704b3763d2df567
Mariano Shaar <a00825287@itesm.mx>
2023-05-11 16:19:13 -0600

    Declaracion de memoria vacia en el directorio

--- directorio_funciones.py
+++ directorio_funciones.py
@@ -1,13 +1,22 @@
+memoria_vacia = {
+                    'total' : 0,
+                    'ent' : 0,
+                    'flot' : 0,
+                    'car' : 0,
+                    'cadena' : 0,
+                    'bool' : 0,
+                }, 
+
 class DirFunciones:
     def __init__(self):
         self.directorio = {
             # nombre : [tipo, variables, parametros, memoria, cuadruplo, return]
-            'hule' : ['void', None, [], {}, None, None]
+            'hule' : ['void', None, [], memoria_vacia, None, None]
         }
     
     def insertar(self, func, tipo):
         if not self.directorio.get(func):
-            self.directorio[func] = [tipo, None, [], {}, None, None]
+            self.directorio[func] = [tipo, None, [], memoria_vacia, None, None]
         else:
             raise Exception("Funcion " + func + " definida multiples veces")
         
