09acb885807820723458340f29c9be6bb3dbf12a
Mariano Shaar <a00825287@itesm.mx>
2023-05-13 13:27:01 -0600

    Funciones para manejar parametros

--- directorio_funciones.py
+++ directorio_funciones.py
@@ -26,19 +26,30 @@ class DirFunciones:
         self.directorio[func][1] = Variables(alcance)
         
     def insertar_variable(self, func, tipo, var=None):
-        self.directorio[func][3][tipo] += 1
-        self.directorio[func][3]['total'] += 1
+        self.directorio[func][3][0][tipo] += 1
+        self.directorio[func][3][0]['total'] += 1
         return self.directorio[func][1].insertar(tipo, var)
 
-    def insertar_parametro(self, func, tipo):
-        self.directorio[func][2].append(tipo)
+    def insertar_parametro(self, func, tipo, var):
+        self.directorio[func][2].append((var, tipo))
+        self.insertar_variable(func, tipo, var)
 
-    def buscar_variable(self, func, id):
+    def buscar_variable(self, func, id, glob=False):
         var = self.directorio[func][1].tabla['var'].get(id)
         if var:
             return var[1]
-        else:
-            raise Exception("Variable " + id + " no definida")
+        if glob:
+            var = self.directorio['hule'][1].tabla['var'].get(id)
+            if var:
+                return var[1]
+        raise Exception("Variable " + id + " no definida")
         
     def tiene_tabla_var(self, func):
         return self.directorio[func][1] != None
+    
+    def comparar_parametro(self, func, pos, arg):
+        param = self.directorio[func][2][pos]
+        if arg != param[1]:
+            raise Exception("Argumento " + str(pos) + " es de tipo " + arg + " y deberia ser " + param[1])
+        else:
+            return self.buscar_variable(func, param[0])
