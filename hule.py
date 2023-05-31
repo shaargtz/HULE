f75bb13f59425751586dcff9165cff7b24bfe64b
Mariano Shaar <a00825287@itesm.mx>
2023-05-31 14:26:04 -0600

    Opcion para ejecutar programas escritos por el usuario

--- hule.py
+++ hule.py
@@ -4,9 +4,13 @@ from src.maquina_virtual import MaquinaVirtual
 from src.utilidad import *
 
 nombre_archivo = input('Escribe el nombre del archivo a correr: ')
-debug = input("¿Quieres imprimir los datos de debug? s/[n] :") or 'n'
+prueba = input("Es un archivo de prueba? [s]/n : ") or 's'
+debug = input("¿Quieres imprimir los datos de debug? s/[n] : ") or 'n'
 
-with open("pruebas/{}.hule".format(nombre_archivo)) as archivo:
+if prueba == 's':
+    nombre_archivo = 'pruebas/' + nombre_archivo
+
+with open("{}.hule".format(nombre_archivo)) as archivo:
     codigo = archivo.read()
 
 parser.parse(codigo)
